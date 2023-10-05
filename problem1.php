352c5953c85b46d8aabcaf2b3e45988eab08b9ed
uberflip-kumaran <kumaran.perinpanatan@uberflip.com>
2023-10-05 15:34:33 -0400

    Add practice problem 1

---
+++ problem1.php
@@ -0,0 +1,14 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd($number) {
+
+}
+
+// Example usage:
+$number1 = 4;
+$result1 = evenOrOdd($number1); // Returns "Even"
+
+$number2 = 7;
+$result2 = evenOrOdd($number2); // Returns "Odd"
+
+// print the results
