e38df46f538edff8c2f431bf5db4264d890ceb10
shuo-uf <129103380+shuo-uf@users.noreply.github.com>
2023-10-19 12:05:45 -0700

    Update problem2.php

--- problem2.php
+++ problem2.php
@@ -6,8 +6,8 @@ function printMinMax($number) {
 
 // Example usage:
 $input1 = array(1, 10, 100);
-$result1 = printMinMax($input1 ); // echo 1
+$result1 = printMinMax($input1 );
 
 $input2 = array(-1, -10, -100);
-$result2 = printMinMax($input2); // echo -100
+$result2 = printMinMax($input2);
 
