d2f258d9f1573a260dbee62696d512a4e011f534
Shuo Chen <shuo.chen@uberflip.com>
2023-10-19 11:43:22 -0700

    add an array related problem

---
+++ problem2.php
@@ -0,0 +1,13 @@
+// Create a function that echos the min and max number of an array
+
+function printMinMax($number) {
+   
+}
+
+// Example usage:
+$input1 = array(1, 10, 100);
+$result1 = printMinMax($input1 ); // echo 1
+
+$input2 = array(-1, -10, -100);
+$result2 = printMinMax($input2); // echo -100
+
